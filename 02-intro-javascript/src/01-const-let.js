4a55837191510aabc2d19b5b180736d9d73276be
Jesus Bolivar <jesus.bolivar@fqtecnolia.com>
2024-04-06 10:25:48 -0500

    S3M4-Variables y constantes

---
+++ 02-intro-javascript/src/01-const-let.js
@@ -0,0 +1,14 @@
+// Variables y constantes
+
+const nombre = 'fernando'
+let apellido = 'Herrera'
+
+let valorDado = 5
+valorDado = 6
+
+console.log(valorDado);
+if (true) {
+    let valorDado = 3
+    console.log(valorDado);
+}
+console.log(valorDado);
