57cd010d68224edacc2a9e9da529a56b74a56658
Jesus Bolivar <jesus.bolivar@fqtecnolia.com>
2024-04-17 14:58:55 -0500

    S3M12-imports-exports-default exports

---
+++ 02-intro-javascript/src/bases/08-import-export.js
@@ -0,0 +1,17 @@
+// import {heroes} from './data/heroes';
+import heroes, {owners} from "../data/heroes"; 
+
+console.log(owners);
+
+
+const getHeroesById = (id) => {
+    return heroes.find( (heroe) => heroe.id === id)
+}
+
+console.log(getHeroesById(3));
+
+const getHeroesByOwner = (owner) => {
+    return heroes.filter( (heroe) => heroe.owner === owner);
+}
+
+console.log(getHeroesByOwner('DC'));
