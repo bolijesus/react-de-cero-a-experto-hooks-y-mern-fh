bafcf99e65f95c7308f58a59d442250c5fc2356b
Jesus Bolivar <jesus.bolivar@fqtecnolia.com>
2024-04-18 11:32:08 -0500

    S3M16-operadores ternarios

---
+++ 02-intro-javascript/src/bases/12-operadores-ternarios.js
@@ -0,0 +1,13 @@
+const activo = true;
+
+// let mensaje = '';
+
+// if(activo){
+//     mensaje = 'Activo';
+// }else{
+//     mensaje = 'Inactivo';
+// }
+// const mensaje = (activo) ? 'Activo' : 'Inactivo';
+
+const mensaje = (activo) && 2+2 == 4 && 'epa'
+console.log(mensaje);
