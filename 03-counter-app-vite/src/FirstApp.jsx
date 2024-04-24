7ef858699769058fc25273307f574b327eb82924
Jesus Bolivar <jesus.bolivar@fqtecnolia.com>
2024-04-24 15:31:38 -0500

    S4M14-15-proptypes y defaultprops

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,11 +1,21 @@
+import PropTypes from 'prop-types'
 
-
-export const FirstApp = () => {
+export const FirstApp = ({ title, subtitle = 'La tierra explota' }) => {
 
     return (
         <>
-            <h1>{'algo'}</h1>
-            <p>Subtitulo</p>
+            <h1>{title}</h1>
+            <p>Subtitulo: {subtitle}</p>
         </>
     );
+}
+
+FirstApp.propTypes = {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string
+}
+
+FirstApp.defaultProps = {
+    title: 'No hay titulo',
+    subtitle: 'KABUM'
 }
