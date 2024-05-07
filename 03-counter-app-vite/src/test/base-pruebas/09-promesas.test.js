dc535b2a84a110686602d315e93912e07449759c
Jesus Bolivar <jesus.bolivar@fqtecnolia.com>
2024-05-07 14:16:36 -0500

    S5M12-Pruebas en tareas asincronas - Pruebas en 09-promesas.js-

---
+++ 03-counter-app-vite/src/test/base-pruebas/09-promesas.test.js
@@ -0,0 +1,24 @@
+import { getHeroeByIdAsync } from "../../base-pruebas/09-promesas";
+
+describe('Pruebas en 09-promesas.js', () => {
+    test('getHeroeByIdAsync debe retornar un heroe', (done) => {
+        const id = 1;
+        getHeroeByIdAsync(id)
+        .then((hero)=>{
+            expect(hero).toEqual({
+                id: 1,
+                name: 'Batman',
+                owner: 'DC'
+            })
+            done();
+        });
+    })
+    test('getHeroeByIdAsync debe retornar un error si heroe no existe', (done) => {
+        const id = 100;
+        getHeroeByIdAsync(id)
+            .catch((error)=>{
+                expect(error).toBe('No se pudo encontrar el héroe');
+                done();
+            });
+    })
+})
